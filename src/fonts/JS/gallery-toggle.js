910d0dd8d8ca38857d1d17c935a73b3fa971e55a
TetiZ <tommytaba99@gmail.com>
2023-12-02 20:39:08 +0100

    about update
    gallery js created, css, html update

---
+++ src/fonts/JS/gallery-toggle.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const gallery = document.querySelector(".about-gallery");
+const images = document.querySelectorAll(".about-img");
+
+const prevBtn = document.querySelector(".prev-img");
+const nextBtn = document.querySelector(".next-img");
+
+let currentIdx = 0;
+const imageCount = images.length;
+
+const showImg = (index) => {
+  images.forEach((img, idx) => {
+    img.classList.toggle("active", idx === index); // Показываем только активное изображение
+  });
+};
+
+const prevImg = () => {
+  currentIdx = (currentIdx - 1 + imageCount) % imageCount;
+  showImg(currentIdx);
+};
+
+const nextImg = () => {
+  currentIdx = (currentIdx + 1) % imageCount;
+  showImg(currentIdx);
+};
+
+prevBtn.addEventListener("click", prevImg);
+nextBtn.addEventListener("click", nextImg);
+
+showImg(currentIdx);
