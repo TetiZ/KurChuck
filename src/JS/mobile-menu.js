e22bc22e6fbd6885f90bd28ea832cea3f51a28bc
TetiZ <tommytaba99@gmail.com>
2023-12-05 02:45:21 +0100

    mobile menu added

---
+++ src/JS/mobile-menu.js
@@ -0,0 +1,18 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    closeMenuLink: document.querySelectorAll(".footer-nav-link"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeMenuLink.forEach((item) =>
+    item.addEventListener("click", toggleModal)
+  );
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
