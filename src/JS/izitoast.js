6e3ff550b8c8819b568d84c877c1be69991d8a4e
TetiZ <tommytaba99@gmail.com>
2023-12-28 10:16:01 +0100

    izitoast

---
+++ src/JS/izitoast.js
@@ -0,0 +1,14 @@
+"use strict";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+iziToast.show({
+  image:
+    "https://imagespulse.com/wp-content/uploads/2022/11/We-Wish-You-a-Merry-Christmas-Images.jpg",
+  imageWidth: 150,
+  position: "center",
+  title: `Merry X-mas!`,
+  message: `Z myślą o dobro waszego biznesu`,
+  layout: 2,
+});
